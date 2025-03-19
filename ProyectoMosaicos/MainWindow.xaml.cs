568ac84e4b964ab8f520dd6195f133e71804d989
csilva-14 <csilva@deuser.es>
2025-03-19 14:44:48 +0100

    Update MainWindow.xaml.cs

--- ProyectoMosaicos/MainWindow.xaml.cs
+++ ProyectoMosaicos/MainWindow.xaml.cs
@@ -1,13 +1,6 @@
-﻿using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
+using ProyectoMosaicos.Models;
 
 namespace ProyectoMosaicos
 {
@@ -16,9 +9,45 @@ namespace ProyectoMosaicos
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        string connectionString = "Server=localhost;Database=mosaicos;Uid=root;";
+
         public MainWindow()
         {
             InitializeComponent();
+            getPaletsCombobox();
         }
+
+        private void getPaletsCombobox() {
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                conn.Open();
+
+                // Realizamos la consulta
+                string query = "SELECT * FROM palet";
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+
+                // Guardamos los palets
+                var listaPalets = new List<Pale>();
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Pale item = new Pale
+                    {
+                        Id = reader.GetInt32(0),
+                        Tipo = reader.GetString(1),
+                        Ancho = reader.GetInt32(2),
+                        Alto = reader.GetInt32(3)
+                    };
+
+                    listaPalets.Add(item);
+                    cbPalet.Items.Add(item.Tipo);
+                }
+
+                conn.Close();
+            }
+        }
+
     }
 }
