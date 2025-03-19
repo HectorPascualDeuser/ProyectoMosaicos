388ebda8b90e74030c525cc9d32267016105725c
HectorPascualDeuser <hpascual@deuser.es>
2025-03-19 13:40:32 +0100

    Inicio de la logica del mosaico

---
+++ ProyectoMosaicos/ClaseAuxiliar.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ProyectoMosaicos
+{
+    public class ClaseAuxiliar
+    {
+        public bool cantidadProductos(int altoPale, int anchoPale, int altoProducto, int anchoProducto)
+        {
+            int numeroProductos;
+            int distanciaReducida;
+            int anchoRestante;
+            int altoRestante;
+            if (0 >= altoProducto || 0 >= altoPale || 0 >= anchoPale || 0 >= anchoProducto) {
+                MessageBox.Show("Ningún valor puede ser 0", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }else if (altoProducto > altoPale && altoProducto > anchoPale || anchoProducto > altoPale && anchoProducto > anchoPale)
+            {
+                MessageBox.Show("El Producto no cabe en el Palet", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+
+            if(altoProducto> anchoProducto && anchoPale > altoPale)
+            {
+                 numeroProductos = division(anchoPale, anchoProducto);
+                 distanciaReducida = numeroProductos * anchoProducto;
+                 anchoRestante = anchoPale - distanciaReducida;
+                
+            }else if (altoProducto < anchoProducto && anchoPale < altoPale)
+            {
+
+            }
+
+
+
+                return true;
+        }
+
+        public int division(int distanciaPale, int distanciaProducto)
+        {
+            return distanciaPale / distanciaProducto;
+        }
+}
+}
+
