a77ad42cbe39de490b931e7dc3e496cb196047a1
HectorPascualDeuser <hpascual@deuser.es>
2025-03-20 08:52:36 +0100

    Update Mosaicos.sql

--- Mosaicos.sql
+++ Mosaicos.sql
@@ -1,30 +1,27 @@
-CREATE DATABASE mosaicos;
+CREATE DATABASE IF NOT EXISTS mosaicos;
 USE mosaicos;
-
 
 # ---------------------------------------------------
 # -- CREACIÓN DE TABLAS
 
-CREATE TABLE palet(
-	id INT AUTO_INCREMENT PRIMARY KEY,
-    tipo VARCHAR(100) NOT NULL,
-    ancho INT NOT NULL,
-    largo INT NOT NULL
+CREATE TABLE palet (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    tipo VARCHAR(100) NOT NULL UNIQUE,
+    ancho INT NOT NULL CHECK (ancho > 0),
+    largo INT NOT NULL CHECK (largo > 0)
 );
 
-CREATE TABLE producto(
-	id INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(100) NOT NULL,
-    ancho INT NOT NULL,
-    largo INT NOT NULL,
-	alto INT NOT NULL
+CREATE TABLE producto (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL UNIQUE,
+    ancho INT NOT NULL CHECK (ancho > 0),
+    largo INT NOT NULL CHECK (largo > 0),
+    alto INT NOT NULL CHECK (alto > 0)
 );
-
 
 # ---------------------------------------------------
 # -- INSERCIÓN DE DATOS
 
-INSERT INTO palet(tipo, ancho, largo) VALUES
+INSERT INTO palet (tipo, ancho, largo) VALUES
     ('Europeo', 1200, 800),
     ('Americano', 1200, 1000);
-
